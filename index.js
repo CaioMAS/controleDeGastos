10425890d2d28603053aee4271640d497ab7167b
Caio Santos <caiosantos.eng@hotmail.com>
2023-02-27 18:18:02 -0300

    Segundo commit

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-let lista = []
+const lista = []
 const item = {}
 
 const calcularTotal = () => {
     const somar = lista.reduce((valorAcumulado, item) => {
-        return Number(valorAcumulado) + Number(item.valorDaCompra)
+        return Number(valorAcumulado) + Number(item.valorDaCompraComSinal)
     }, 0)
-    return somar   
+    return somar
 }
 
-const createTable = (dataDaCompra, classificacaoDaCompra, descricaoDaCompra,valorDaCompra) => {
+const createTable = (dataDaCompra, classificacaoDaCompra, descricaoDaCompra, valorDaCompra) => {
     const bodyTable = document.getElementById('bodyTable')
-    const newRow = document.createElement('tr')
+    const newRow = document.createElement('tr')    
+    newRow.id = descricaoDaCompra + valorDaCompra
+    
 
-    const newColumnDate = document.createElement('td') 
-    newColumnDate.classList = 'columHead' 
+    const newColumnDate = document.createElement('td')
+    newColumnDate.classList = 'columnBody'
     newColumnDate.innerText = dataDaCompra
 
-    const newColumnClass = document.createElement('td') 
-    newColumnClass.classList = 'columHead'  
+    const newColumnClass = document.createElement('td')
+    newColumnClass.classList = 'columnBody'
     newColumnClass.innerText = classificacaoDaCompra
 
-    const newColumnDescription = document.createElement('td')   
-    newColumnDescription.classList = 'columHead' 
+    const newColumnDescription = document.createElement('td')
+    newColumnDescription.classList = 'columnBody'
     newColumnDescription.innerText = descricaoDaCompra
 
     const newColumnValue = document.createElement('td')
-    newColumnValue.classList = 'columHead'
+    newColumnValue.classList = 'columnBody'
     newColumnValue.innerText = valorDaCompra
 
     const newColumnEdit = document.createElement('td')
@@ -39,44 +41,75 @@ const createTable = (dataDaCompra, classificacaoDaCompra, descricaoDaCompra,valo
 
     const buttonRemove = document.createElement('button')
     buttonRemove.classList = 'remove-button'
-    buttonRemove.innerHTML = 'X'      
+    buttonRemove.innerHTML = 'X'
 
     bodyTable.appendChild(newRow)
     newRow.append(newColumnDate, newColumnClass, newColumnDescription, newColumnValue, newColumnEdit, newColumnRemove)
     newColumnEdit.appendChild(buttonEdit)
     newColumnRemove.appendChild(buttonRemove)
-    
+
 }
 
 document.addEventListener('click', (e) => {
 
     const targetEl = e.target
-    const parentEl = targetEl.closest('tr')    
+    const parentEl = targetEl.closest('tr')
 
-    if(targetEl.classList.contains("remove-button")) {
+    if (targetEl.classList.contains("remove-button")) {
+        
         parentEl.remove()
     }
 })
 
+
 document.getElementById('btnAddItem').addEventListener('click', (ev) => {
+
     ev.preventDefault()
+    
     const dataDaCompra = document.getElementById('dataDoItem').value
     const classificacaoDaCompra = document.getElementById('classificacaoDoItem').value
     const descricaoDaCompra = document.getElementById('descricaoDoItem').value
     const valorDaCompra = document.getElementById('valorDoItem').value
+    const inputEntradaSaida = document.getElementsByName('entradaSaida')
+    let valorDaCompraComSinal = 0
 
+    for(let i = 0; i < inputEntradaSaida.length; i++){
+        if(inputEntradaSaida[i].checked) {
+            var inputChecked = inputEntradaSaida[i].value
+        }   
+    
+    }
+
+    switch(inputChecked) {
+        case "entrada": 
+            valorDaCompraComSinal = valorDaCompra
+            break
+        case "saida":
+            valorDaCompraComSinal = valorDaCompra * (-1)
+            break
+    }
+
+    
+    const item = {}           
+        
     item.dataDaCompra = dataDaCompra
     item.classificacaoDaCompra = classificacaoDaCompra
     item.descricaoDaCompra = descricaoDaCompra
-    item.valorDaCompra = valorDaCompra
-
-    lista.push(item)
+    item.valorDaCompraComSinal = valorDaCompraComSinal   
     
-    console.log(lista)
-    calcularTotal()
-    createTable(dataDaCompra, classificacaoDaCompra, descricaoDaCompra, valorDaCompra)
-    cleanInput()
-    console.log(calcularTotal())     
+    
+
+    if (dataDaCompra == "" || classificacaoDaCompra == "" || descricaoDaCompra == "" || valorDaCompraComSinal == "") {
+        alert ("Favor preencher todos os dados")    
+    } else {         
+        lista.push(item)  
+        console.table(lista)            
+        createTable(dataDaCompra, classificacaoDaCompra, descricaoDaCompra, valorDaCompraComSinal)        
+        cleanInput() 
+        
+    }   
+    calcularTotal() 
+    console.log(calcularTotal())
 })
 
 const cleanInput = () => {
@@ -84,9 +117,11 @@ const cleanInput = () => {
     classificacaoDaCompra = document.getElementById('classificacaoDoItem').value = ""
     descricaoDaCompra = document.getElementById('descricaoDoItem').value = ""
     valorDaCompra = document.getElementById('valorDoItem').value = ""
+    const inputEntradaSaida = document.getElementsByName('entradaSaida').value = ""
     dataDaCompra = document.getElementById('dataDoItem').focus()
-    
-} 
+
+}
+
 
 
 
