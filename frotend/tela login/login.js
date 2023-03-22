d8332a1e62b268d0a57115417aa5eb71b725679f
Caio Santos <caiosantos.eng@hotmail.com>
2023-03-22 09:50:05 -0300

    Alterações nos arquivos de tela login

--- frotend/tela login/login.js
+++ frotend/tela login/login.js
@@ -1,14 +1,43 @@
-function validateFields() {
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+}
+
+function login() {
+    window.location.href = "pages/home/home.html"
+}
+
+function register() {
+    window.location.href = "pages/register/register.html"
+}
+
+function toggleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+}
+
+function togglePasswordErrors() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonsDisable() {
     const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
+    form.recoverPasswordButton().disabled = !emailValid;
 
     const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
-
+    form.loginButton().disabled = !emailValid || !passwordValid;
 }
 
 function isEmailValid() {
-    const email = document.getElementById("email").value;
+    const email = form.email().value;
     if (!email) {
         return false;
     }
@@ -16,13 +45,15 @@ function isEmailValid() {
 }
 
 function isPasswordValid() {
-    const password = document.getElementById("password").value;
-    if (!password) {
-        return false;
-    }
-    return true;
+    return form.password().value ? true : false;
 }
 
-function validateEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-}
+const form = {
+    email: () => document.getElementById("email"),
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
+    emailRequiredError: () => document.getElementById("email-required-error"),
+    loginButton: () => document.getElementById("login-button"),
+    password: () => document.getElementById("password"),
+    passwordRequiredError: () => document.getElementById("password-required-error"),
+    recoverPasswordButton: () => document.getElementById("recover-password-button"),
+} 
